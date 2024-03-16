33514c34d2288df64ae8e5c3bbf9972081ac54fc
Waseque Arafat <waseque.ndc@gmail.com>
2024-03-16 13:34:08 +0600

    design almost finish

---
+++ src/Pages/Home/Servises/Servises.jsx
@@ -0,0 +1,19 @@
+import React, { useEffect, useState } from "react";
+
+const Servises = () => {
+  return (
+    <div>
+      <div className="text-center">
+        <h3 className="text-3xl text-orange-600">Our Servises</h3>
+        <h3 className="text-6xl font-semibold   ">Our Servises Area</h3>
+        <p>
+          the majority have suffered alteration in some form, by injected
+          humour, or randomised <br /> words which don't look even slightly
+          believable.{" "}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Servises;
